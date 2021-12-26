f5a8b8177013383aa697f68deb70ece8e90c279e
RaGe <foragerr@gmail.com>
2021-12-26 05:16:49 -0500

    Get transaction data from api

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -7,64 +7,9 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Title from './Title';
 
-// Generate Order Data
-function createData(id, date, direction, name, shipTo, paymentMethod, amount) {
-  return { id, date, direction, name, shipTo, paymentMethod, amount };
-}
 
-const rows = [
-  createData(
-    0,
-    '16 Mar, 2019',
-    'In',
-    'NYC',
-    '0x9736a6d28481ff303a3dbaf75ad1b82d4ae0afca20c9e79d81cf8064d3b311e3',
-    200000,
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'In',
-    'MIA',
-    '0x9736a6d28481ff303a3dbaf75ad1b82d4ae0afca20c9e79d81cf8064d3b311e3',
-    40000,
-    866.99,
-  ),
-  createData(
-    2, 
-    '16 Mar, 2019',
-    'Out',
-    'DIKO', 
-    '0x9736a6d28481ff303a3dbaf75ad1b82d4ae0afca20c9e79d81cf8064d3b311e3', 
-    300000, 
-    100.81
-  ),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Out',
-    'STX',
-    '0x9736a6d28481ff303a3dbaf75ad1b82d4ae0afca20c9e79d81cf8064d3b311e3',
-    4000,
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Out',
-    'BTC',
-    '0x9736a6d28481ff303a3dbaf75ad1b82d4ae0afca20c9e79d81cf8064d3b311e3',
-    3,
-    212.79,
-  ),
-];
+export default function Transactions({txnData}) {
 
-function preventDefault(event) {
-  event.preventDefault();
-}
-
-export default function Transactions() {
   return (
     <React.Fragment>
       <Title>Transactions</Title>
@@ -76,23 +21,23 @@ export default function Transactions() {
             <TableCell>Symbol</TableCell>
             <TableCell>Tx ID</TableCell>
             <TableCell>Quantity</TableCell>
-            <TableCell align="right">in USD</TableCell>
+            <TableCell align="right">Price</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {txnData.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
+              <TableCell>{row.burnDate}</TableCell>
               <TableCell>{row.direction}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
+              <TableCell>{row.inSymbol}</TableCell>
+              <TableCell>{row.xactnId}</TableCell>
+              <TableCell>{row.inAmount}</TableCell>
+              <TableCell align="right">{`$${row.inCoinPrice}`}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+      <Link color="primary" href="#" sx={{ mt: 3 }}>
         Export to CSV
       </Link>
     </React.Fragment>
--- src/views/TxReport.js
+++ src/views/TxReport.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
@@ -6,8 +7,35 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 import Transactions from '../components/Transactions';
+import convertJsonToOutputArray from '../api/convertapijsontooutputarray'
+import processAllXactnWithTransfersApiPages from '../api/stxapicalls'
 
 export default function TxReport() {
+
+  const [walletId, setWalletId] = useState('');
+  const [txnData, setTxnData] = useState([]);
+
+  const handleWalletIdChange = (event) => {
+    setWalletId(event.target.value);
+  };
+
+  async function getWalletTxData() {
+
+    console.log(walletId);
+  
+    if (walletId.length<5) // TODO: need a more robust check - perhaps against explorer API?
+    {
+        alert("Please enter a valid wallet address");
+        return;
+    }
+  
+    let json= await processAllXactnWithTransfersApiPages(walletId);
+    let outputArray=await convertJsonToOutputArray(json,walletId);
+    setTxnData(outputArray)
+    return;
+  
+  }
+
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
       <Grid container spacing={3}>
@@ -36,9 +64,12 @@ export default function TxReport() {
                   required
                   id="fullWidth"
                   label="Wallet Address"
+                  onChange={ handleWalletIdChange }
                 />
 
-                <Button sx={{mt: 2, ml:2}} variant="contained">Go</Button>
+                <Button sx={{mt: 2, ml:2}} variant="contained" onClick={ getWalletTxData }>
+                  Go
+                </Button>
               </div>
             </Box>
           </Paper>
@@ -47,7 +78,7 @@ export default function TxReport() {
         {/* Transactions*/}
         <Grid item xs={12}>
           <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-            <Transactions />
+            <Transactions txnData = {txnData} />
           </Paper>
         </Grid>
       </Grid>
