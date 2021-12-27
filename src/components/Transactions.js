deb1125eda80cb9edc0f6898d2da9de3e88acba1
RaGe <foragerr@gmail.com>
2021-12-27 13:32:00 -0500

    format date, use tooltip for full date

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -5,6 +5,8 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import Tooltip from '@mui/material/Tooltip';
 import Title from './Title';
 
 
@@ -28,6 +30,16 @@ export default function Transactions({txnData}) {
     return transactionInWords
   };
 
+  const toDateString = (dateString) => {
+    var d = new Date (dateString)
+    return d.toDateString();
+  }
+
+  const toUTCString = (dateString) => {
+    var d = new Date (dateString)
+    return d.toUTCString();
+  }
+
   return (
     <React.Fragment>
       <Title>Transactions</Title>
@@ -37,22 +49,34 @@ export default function Transactions({txnData}) {
             <TableCell>Date</TableCell>
             <TableCell>Transaction</TableCell>
             <TableCell>TxID</TableCell>
-            <TableCell align="right">Price</TableCell>
+            <TableCell>In Price</TableCell>
+            <TableCell align="right">Out Price</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {txnData.map((row) => (
             <TableRow key={row.rowId}>
-              <TableCell>{row.burnDate}</TableCell>
+              
+              <Tooltip title={toUTCString(row.burnDate)} arrow>
+                <TableCell>
+                  {toDateString(row.burnDate)}
+                </TableCell>
+              </Tooltip>
+
               <TableCell>{wordifyTransaction(row)}</TableCell>
+
               <TableCell>
                 <a 
                   href = {"https://explorer.stacks.co/txid/" + row.xactnId + "?chain=mainnet"}
                 >
                   {row.xactnId.substring(0, 4) + '...' + row.xactnId.slice(-6) }
                 </a>
+                <ContentCopyIcon />
               </TableCell>
-              <TableCell align="right">{`$${row.inCoinPrice + '//' + row.outCoinPrice}`}</TableCell>
+
+              <TableCell align="right">{`$${row.inCoinPrice}`}</TableCell>
+              <TableCell align="right">{`$${row.outCoinPrice}`}</TableCell>
+
             </TableRow>
           ))}
         </TableBody>
