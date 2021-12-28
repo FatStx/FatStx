d4b165456ea71a5cef4f390a4ebd15d9cb1ec3de
eparrot <eparrot1@yahoo.com>
2021-12-27 19:11:47 -0500

    update calculationFactor for LP tokens

--- src/api/tokens.js
+++ src/api/tokens.js
@@ -6,7 +6,7 @@ export class Token {
       {symbol:'NYC',contract:'SP2H8PY27SEZ03MWRKS5XABZYQN17ETGQS3527SA5.newyorkcitycoin-token::newyorkcitycoin',conversionFactor:1},
       {symbol:'SXSW',contract:'SP1Z92MPDQEWZXW36VX71Q25HKF5K2EPCJ304F275.stsw-token-v4a::stsw',conversionFactor:1},
       {symbol:'xBTC',contract:'SP3DX3H4FEYZJZ586MFBS25ZW3HZDMEW92260R2PR.Wrapped-Bitcoin::wrapped-bitcoin',conversionFactor:100000000},
-      {symbol:'DIKOLPSTX_USDA',contract:'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.arkadiko-swap-token-wstx-usda::wstx-usda',conversionFactor:1},
-      {symbol:'DIKOLPSTX_WBTC',contract:'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.arkadiko-swap-token-wstx-xbtc::wstx-xbtc',conversionFactor:1}
+      {symbol:'DIKOLPSTX_USDA',contract:'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.arkadiko-swap-token-wstx-usda::wstx-usda',conversionFactor:1000000},
+      {symbol:'DIKOLPSTX_WBTC',contract:'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.arkadiko-swap-token-wstx-xbtc::wstx-xbtc',conversionFactor:1000000}
     ];
 }
