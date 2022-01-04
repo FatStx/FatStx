c5030b4050e9df01f95d40e7699f4da826e6e2c8
RaGe <foragerr@gmail.com>
2022-01-04 15:43:42 -0500

    remove unused import

--- src/views/StacksBoard.js
+++ src/views/StacksBoard.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route, Routes, useParams } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
