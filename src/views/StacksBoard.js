9e7327090d10bf71ff1644849acd03f23fa56249
RaGe <foragerr@gmail.com>
2022-01-06 17:02:14 -0500

    Use default appbar component, customization not necessary

--- src/views/StacksBoard.js
+++ src/views/StacksBoard.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { Route, Routes, Link as RouterLink } from 'react-router-dom';
 
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import MuiAppBar from '@mui/material/AppBar';
+import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
@@ -17,24 +17,6 @@ import StackingReport from './StackingReport'
 import WenBlok from './WenBlok'
 import Disclaimer from './Disclaimer'
 
-const drawerWidth = 190;
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
 
 const mdTheme = createTheme();
 
