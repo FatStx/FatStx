0f6d98a3f7f4b1ebf8735b9de76fe4c55471154b
eparrot <eparrot1@yahoo.com>
2022-01-07 08:42:12 -0500

    remove temp hardcoded values from MIA satcking list

--- src/api/stacking/cities/miastackinglist.js
+++ src/api/stacking/cities/miastackinglist.js
@@ -3,11 +3,11 @@ export class MIAStackingList
 {
     static localStackingList = [
         { round: 0, startBlock: 24497, endBlock: 26596, endBlockDate: "2021-08-20T05:03:09.000Z", stackedCoins: 0, claimedRewards: 0, claimDate: "", canClaimCoin: ""  },
-        { round: 1, startBlock: 26597, endBlock: 28696, endBlockDate: "2021-09-05T14:52:42.000Z", stackedCoins: 10000, claimedRewards: 50, claimDate: "2021-09-22T20:18:03.000Z", canClaimCoin: "STX Only"  },
-        { round: 2, startBlock: 28697, endBlock: 30796, endBlockDate: "2021-09-21T22:18:03.000Z", stackedCoins: 20000, claimedRewards: 62, claimDate: "2021-10-09T22:23:47.000Z", canClaimCoin: "STX+Coin"  },
-        { round: 3, startBlock: 30797, endBlock: 32896, endBlockDate: "2021-10-07T22:23:47.000Z", stackedCoins: 10000, claimedRewards: 0, claimDate: "", canClaimCoin: "STX Only"  },
-        { round: 4, startBlock: 32897, endBlock: 34996, endBlockDate: "2021-10-24T09:22:47.000Z", stackedCoins: 10000, claimedRewards: 0, claimDate: "", canClaimCoin: "STX Only"  },
-        { round: 5, startBlock: 34997, endBlock: 37096, endBlockDate: "2021-11-09T02:17:56.000Z", stackedCoins: 10000, claimedRewards: 0, claimDate: "", canClaimCoin: "STX+Coin"  },
+        { round: 1, startBlock: 26597, endBlock: 28696, endBlockDate: "2021-09-05T14:52:42.000Z", stackedCoins: 0, claimedRewards: 50, claimDate: "", canClaimCoin: ""  },
+        { round: 2, startBlock: 28697, endBlock: 30796, endBlockDate: "2021-09-21T22:18:03.000Z", stackedCoins: 0, claimedRewards: 62, claimDate: "", canClaimCoin: ""  },
+        { round: 3, startBlock: 30797, endBlock: 32896, endBlockDate: "2021-10-07T22:23:47.000Z", stackedCoins: 0, claimedRewards: 0, claimDate: "", canClaimCoin: ""  },
+        { round: 4, startBlock: 32897, endBlock: 34996, endBlockDate: "2021-10-24T09:22:47.000Z", stackedCoins: 0, claimedRewards: 0, claimDate: "", canClaimCoin: ""  },
+        { round: 5, startBlock: 34997, endBlock: 37096, endBlockDate: "2021-11-09T02:17:56.000Z", stackedCoins: 0, claimedRewards: 0, claimDate: "", canClaimCoin: ""  },
         { round: 6, startBlock: 37097, endBlock: 39196, endBlockDate: "2021-11-25T19:42:21.000Z", stackedCoins: 0, claimedRewards: 0, claimDate: "", canClaimCoin: ""  },
         { round: 7, startBlock: 39197, endBlock: 41296, endBlockDate: "2021-12-12T21:11:31.000Z", stackedCoins: 0, claimedRewards: 0, claimDate: "", canClaimCoin: ""  },
         { round: 8, startBlock: 41297, endBlock: 43396, endBlockDate: "2021-12-31T15:48:14.000Z", stackedCoins: 0, claimedRewards: 0, claimDate: "", canClaimCoin: ""  },
