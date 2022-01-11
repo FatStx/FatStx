bdee700994ce9e3ffa3496c9adf48ab46772e038
eparrot <eparrot1@yahoo.com>
2022-01-11 09:53:35 -0500

    Finish draft of disclaimer and FAQ

--- src/views/Disclaimer.js
+++ src/views/Disclaimer.js
@@ -23,8 +23,13 @@ export default function Disclaimer() {
             <ul><li>The transaction report and exported CSVs may be useful in filling out US Income tax returns. 
                 Notes about the possible treatment of transactions are intended solely for discussion and do not constitute tax or legal advice.</li></ul>
             <h2>About</h2>
-
             <p> Built by @eparrot and @foragerr</p>
+            <h2>License</h2>
+            <h3>StacksBoard</h3>
+            <h3>Copyright 2022 under an MIT license</h3>
+            <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+            <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+            <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
         </Container>
     )
 }
--- src/views/Faq.js
+++ src/views/Faq.js
@@ -9,16 +9,28 @@ export default function Faq() {
     return(
         <Container >
             <h1>StacksBoard FAQ</h1>
-            <ul>
-                <li>The site is taking forever to load, what do I do?</li>
+                <h2>The site is taking forever to load, what do I do?</h2>
                 Stacks API calls are slow sometimes! If it takes more than 30 seconds, close and try again a minute later. Do not keep spamming the refresh button.
-                <li>Currently the page only accepts the raw addresses, not .btc addresses.</li>
-                <li>Tell me about the Transaction dates</li>
-                    Transaction Dates are in UTC time, which is the standard time that many tax sites require. In the csv file, the dates are specifically in the ISO time format
-                <li>How about the Stacking dates and WenBlok dates?</li>
-                    Stacking and WenBlok dates are in your local browser time zone. Future dates are estimates and may be off by several days
-                <li>How do you calculate the coin prices?</li>
-            </ul>
+                <h2>I put in a valid wallet address and it doesn't work</h2>
+                Currently the page only accepts the raw addresses, not .btc addresses.
+                <h2>Tell me about the Transaction dates</h2>
+                Transaction Dates are in UTC time, which is the standard time that many tax sites require. In the csv file, the dates are specifically in the ISO time format
+                <h2>How about the Stacking dates and WenBlok dates?</h2>
+                Stacking and WenBlok dates are in your local browser time zone. Future dates are estimates and may be off by several days
+                <h2>How do you calculate the coin prices?</h2>
+                The Stacks ecosystem is new enough that aside from the price of STX it can be difficult to determine accurate coin prices. Sourecing of coin prices is likely something
+                which will be changing regularly as more accurate sources become available. As of January 11, 2022 coin prices were detrmined as follows
+                <ul>
+                    <li><strong>STX: </strong> 2021 prices are the most recent hourly price from Binance historical data. 2022 prices are the daily historical prices from CoinGecko</li>
+                    <li><strong>MIA: </strong> All prices are the the daily historical prices from CoinGecko</li>
+                    <li><strong>USDA: </strong> 2021 Prices are derived from the STX-USDA swap contract on Arkadiko, converting the USDA price in STX to the USDA price in USDT using the hourly Binance historical STX prices.
+                    2022 prices will be derived as each month ends and the Binance prices become available</li>
+                    <li><strong>DIKO: </strong> 2021 Prices are derived from the USDA-DIKO swap contract on Arkadiko, converting the DIKO price in USDA to the DIKO price in USDT using the USDA prices mentioned in the previous bullet.
+                    2022 prices are the the daily historical prices from CoinGecko</li>
+                    <li><strong>xBTC: </strong> Since xBTC prices are driven by BTC prices - xBTC can be unwrapped and exchanged for BTC - currently BTC prices are the most accurate price to use for xBTC.
+                    This is certainly a matter for debate, but that is the approach this site has taken. 2021 prices are the most recent hourly price from Binance historical data. 2022 prices are the daily historical prices from CoinGecko</li>
+                </ul>
+
         </Container>
     )
 }
