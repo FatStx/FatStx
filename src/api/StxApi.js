e890117f326d6e5b24758b9717cd452f26b726ce
eparrot <eparrot1@yahoo.com>
2022-01-17 11:46:12 -0500

    finish price filtering logic

--- src/api/StxApi.js
+++ src/api/StxApi.js
@@ -2,29 +2,28 @@ import { Configuration, BlocksApi } from "@stacks/blockchain-api-client";
 
 //TODO: Proper Error handling, possibly chain then and catch to the fetch
 //Process All API Pages
-export default async function processAllXactnWithTransfersApiPages(walletId,startDate='2021-12-27T00:00:00.000Z',endDate='2023-01-01T00:00:00.000Z') {
+export default async function processAllXactnWithTransfersApiPages(walletId,startDate='2021-01-01T00:00:00.000Z',endDate='2021-10-05T00:00:00.000Z') {
     console.log(Date.now() + " ===Process All Api Pages===");
 
     let apiResult = await processOneXactnWithTransfersApiPage(0, walletId);
 
     let runningJson = apiResult[1].results;
-    let filteredArray=filterDates(runningJson,startDate,endDate);
-    let totalTransactions = apiResult[1].total;
+    let filterResults=filterDates(runningJson,startDate,endDate);
+    runningJson=filterResults[1];
+    if (!filterResults[0]) {
+        let totalTransactions = apiResult[1].total;
 
-    //Loop Through all results after getting first page
-    if (filteredArray.length ===50) {
-
+        //Loop Through all results after getting first page
         for (let i = 50; i <= totalTransactions; i += 50) {
             apiResult = await processOneXactnWithTransfersApiPage(i, walletId);
             //TODO: what if apiResult doesn't return what is expected?
-            let filteredArray=filterDates(apiResult[1].results,startDate,endDate);
-            runningJson = runningJson.concat(filteredArray);
-            if (filteredArray.length !== 50) {
+            let filterResults=filterDates(apiResult[1].results,startDate,endDate);
+            runningJson = runningJson.concat(filterResults[1]);
+            if (filterResults[0]) {
                 break;
             }
         }
     }
-
     return runningJson;
 }
 
@@ -76,14 +75,24 @@ async function processOneApiPage(url) {
 
 function filterDates(json,startDate,endDate) {
     let outputArray = [];
+    let isComplete=true;
+    let isAfterStartDate=false;
     for (const xactn of json) {
         let xactnDate= xactn.tx.burn_block_time_iso;
-        let isInRange=new Date(startDate)<=new Date(xactnDate);
-        if (isInRange) {
-            outputArray.push(xactn);
+        isAfterStartDate=new Date(startDate)<=new Date(xactnDate);
+        let isBeforeEndDate=new Date(endDate)>=new Date(xactnDate);
+        if (isAfterStartDate) {
+            if (isBeforeEndDate)
+            {
+                outputArray.push(xactn);
+            }
         } else {
             break;
         }
     }
-    return outputArray;
+
+    if (isAfterStartDate) {
+        isComplete=false;
+    }
+    return [isComplete,outputArray];
 }
