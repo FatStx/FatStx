ddea2e4d1c037f6cf3781eb00d811a52a82df3d8
eparrot <eparrot1@yahoo.com>
2022-12-27 11:57:20 -0500

    update faq

--- src/views/Faq.js
+++ src/views/Faq.js
@@ -123,7 +123,7 @@ export default function Faq() {
                     As of February 26, 2022 coin prices were determined as follows:
 
                     <ul>
-                        <li><strong>STX: </strong> There are no prices before 1/1/21. Prices after that and before 11/25/21 are the daily prices from CoinGecko. Prices from 11/25/21 through 05/13/22 are the hourly prices from okcoin. Prices from 5/13/22 through 10/26/22 are from hourly Binance historical prices. Prices after 12/24/22 are the daily prices from CoinGecko</li>
+                        <li><strong>STX: </strong> There are no prices before 1/1/21. Prices after that and before 11/25/21 are the daily prices from CoinGecko. Prices from 11/25/21 through 05/13/22 are the hourly prices from okcoin. Prices from 1/1/22 through 12/25/22 are from hourly Binance historical prices. Prices after 12/25/22 are the daily prices from CoinGecko</li>
                         <li><strong>MIA: </strong>From 8/26/21 to 11/24/21, prices are the the daily prices from CoinGecko. Prices from 11/25/21 through 2/25/22 are the hourly prices from okcoin. Prices after 2/25/22 are the daily prices from CoinGecko</li>
                         <li><strong>USDA: </strong> 2021 Prices are derived from the STX-USDA swap contract on Arkadiko, converting the USDA price in STX to the USDA price in USDT using the hourly Binance historical STX prices.
                         2022 prices will be derived as STX-USDA swap contract info becomes available and each month ends and the Binance prices become available</li>
