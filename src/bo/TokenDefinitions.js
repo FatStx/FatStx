c39b83883ca3cdc092b60400fd4b5a7c3f0ef27c
eparrot <eparrot1@yahoo.com>
2023-01-02 16:40:52 -0500

    update decimal displays for some tokens

--- src/bo/TokenDefinitions.js
+++ src/bo/TokenDefinitions.js
@@ -1,6 +1,6 @@
 export class Token {
     static tokens = [
-      {symbol:'STX',apiSymbol:'blockstack',contract:'',conversionFactor:1000000,amountDecimals: 2,priceDecimals: 2},
+      {symbol:'STX',apiSymbol:'blockstack',contract:'',conversionFactor:1000000,amountDecimals: 2,priceDecimals: 3},
       {symbol:'DIKO',apiSymbol:'arkadiko-protocol',contract:'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.arkadiko-token::diko',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 3},
       {symbol:'USDA',apiSymbol:'',contract:'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.usda-token::usda',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 3},
       {symbol:'MIA',apiSymbol:'miamicoin',contract:'SP466FNC0P7JWTNM2R9T199QRZN1MYEDTAR0KP27.miamicoin-token::miamicoin',conversionFactor:1,amountDecimals: 2,priceDecimals: 5},
@@ -17,10 +17,10 @@ export class Token {
       {symbol:'DIKOLP_STX_DIKO',apiSymbol:'',contract:'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.arkadiko-swap-token-wstx-diko::wstx-diko',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 2},
       {symbol:'stDIKO',apiSymbol:'',contract:'SP2C2YFP12AJZB4MABJBAJ55XECVS7E4PMMZ89YZR.stdiko-token::stdiko',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 2},
       {symbol:'idoALEX',apiSymbol:'',contract:'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.lottery-ido-alex::lottery-ido-alex',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 2},
-      {symbol:'WELSH',apiSymbol:'',contract:'SP3NE50GEXFG9SZGTT51P40X2CKYSZ5CC4ZTZ7A2G.welshcorgicoin-token::welshcorgicoin',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 2},
-      {symbol:'BAN',apiSymbol:'',contract:'SP2KAF9RF86PVX3NEE27DFV1CQX0T4WGR41X3S45C.btc-monkeys-bananas::BANANA',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 2},
-      {symbol:'SLIME',apiSymbol:'',contract:'SP125J1ADVYWGWB9NQRCVGKYAG73R17ZNMV17XEJ7.slime-token::SLIME',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 2},
-      {symbol:'ALEX',apiSymbol:'',contract:'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.age000-governance-token::alex',conversionFactor:100000000,amountDecimals: 3,priceDecimals: 2},
+      {symbol:'WELSH',apiSymbol:'',contract:'SP3NE50GEXFG9SZGTT51P40X2CKYSZ5CC4ZTZ7A2G.welshcorgicoin-token::welshcorgicoin',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 4},
+      {symbol:'BAN',apiSymbol:'',contract:'SP2KAF9RF86PVX3NEE27DFV1CQX0T4WGR41X3S45C.btc-monkeys-bananas::BANANA',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 4},
+      {symbol:'SLIME',apiSymbol:'',contract:'SP125J1ADVYWGWB9NQRCVGKYAG73R17ZNMV17XEJ7.slime-token::SLIME',conversionFactor:1000000,amountDecimals: 3,priceDecimals: 4},
+      {symbol:'ALEX',apiSymbol:'',contract:'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.age000-governance-token::alex',conversionFactor:100000000,amountDecimals: 3,priceDecimals: 4},
       {symbol:'APOWER',apiSymbol:'',contract:'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.token-apower::apower',conversionFactor:100000000,amountDecimals: 3,priceDecimals: 2},
       {symbol:'ALEXLP_STX-ALEX',apiSymbol:'',contract:'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.fwp-wstx-alex-50-50-v1-01::fwp-wstx-alex-50-50-v1-01',conversionFactor:100000000,amountDecimals: 3,priceDecimals: 2},
       {symbol:'ALEXLP_STX-ALEX',apiSymbol:'',contract:'SP3K8BC0PPEVCV7NZ6QSRWPQ2JE9E5B6N3PA0KBR9.fwp-wstx-alex-50-50::fwp-wstx-alex-50-50',conversionFactor:100000000,amountDecimals: 3,priceDecimals: 2},
