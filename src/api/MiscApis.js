6d38cc506c22e1a26d1697a89cb4dffaa5b2804c
eparrot <eparrot1@yahoo.com>
2023-03-18 11:02:02 -0400

    try new bitcoin api

--- src/api/MiscApis.js
+++ src/api/MiscApis.js
@@ -2,13 +2,16 @@ import processOneApiPage from '../api/ApiShared'
 
 export async function getLatestBitcoinBlock() {
     //const url = 'https://blockchain.info/latestblock';
+    const url = 'https://api.blockcypher.com/v1/btc/main';
     //SOURCE:https://explorer.btc.com/btc/adapter?type=api-doc
-    const url = 'https://chain.api.btc.com/v3/block/latest/tx?verbose=1'
+    //const url = 'https://chain.api.btc.com/v3/block/latest/tx?verbose=1'
     const res = await processOneApiPage(url);
     if (res[0] === 200) {
+        console.log(res[1].height);
         //return res[1].data.height;
-        const blockHeight=res[1].data["list"][0].block_height;
-        return blockHeight;
+        return res[1].height;
+        //const blockHeight=res[1].data["list"][0].block_height;
+        //return blockHeight;
     }
     return null;
 }
